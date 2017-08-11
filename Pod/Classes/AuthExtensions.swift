31b6216e80a5bffc648f7e719ff0c2aaaa0b0c11
Michael Kuck <me@michael-kuck.com>
2017-08-11 16:03:17 +0800

    Replace Observables by Singles and Completables where possible

--- Pod/Classes/AuthExtensions.swift
+++ Pod/Classes/AuthExtensions.swift
@@ -6,6 +6,8 @@
 import FirebaseAuth
 import RxSwift
 
+public let ErrorCodeUnknownError = 9000
+
 public extension Auth {
     // MARK: - Observable authentication state
     var rx_authState: Observable<User?> {
@@ -23,97 +25,92 @@ public extension Auth {
     }
 
     // MARK: - Signing in
-    func rx_signInWithEmail(email: String, password: String) -> Observable<User?> {
-        return Observable.create { (observer: AnyObserver<User?>) -> Disposable in
-            self.signIn(withEmail: email, password: password, completion: Auth.rx_authCallback(observer: observer))
+    func rx_signInWithEmail(email: String, password: String) -> Single<User> {
+        return Single<User>.create { single in
+            self.signIn(withEmail: email, password: password, completion: Auth.rx_authCallback(single: single))
             return Disposables.create()
         }
     }
 
-    func rx_signInAnonymously() -> Observable<User?> {
-        return Observable.create { (observer: AnyObserver<User?>) -> Disposable in
-            self.signInAnonymously(completion: Auth.rx_authCallback(observer: observer))
+    func rx_signInAnonymously() -> Single<User> {
+        return Single<User>.create { single in
+            self.signInAnonymously(completion: Auth.rx_authCallback(single: single))
             return Disposables.create()
         }
     }
 
-    func rx_signInWithCredential(credential: AuthCredential) -> Observable<User?> {
-        return Observable.create { (observer: AnyObserver<User?>) -> Disposable in
-            self.signIn(with: credential, completion: Auth.rx_authCallback(observer: observer))
+    func rx_signInWithCredential(credential: AuthCredential) -> Single<User> {
+        return Single<User>.create { single in
+            self.signIn(with: credential, completion: Auth.rx_authCallback(single: single))
             return Disposables.create()
         }
     }
 
-    func rx_signInWithCustomToken(customToken: String) -> Observable<User?> {
-        return Observable.create { (observer: AnyObserver<User?>) -> Disposable in
-            self.signIn(withCustomToken: customToken, completion: Auth.rx_authCallback(observer: observer))
+    func rx_signInWithCustomToken(customToken: String) -> Single<User> {
+        return Single<User>.create { single in
+            self.signIn(withCustomToken: customToken, completion: Auth.rx_authCallback(single: single))
             return Disposables.create()
         }
     }
 
     // MARK: - Registering and resetting password
-    func rx_createUserWithEmail(email: String, password: String) -> Observable<User?> {
-        return Observable.create { (observer : AnyObserver<User?>) -> Disposable in
-            self.createUser(withEmail: email, password: password, completion: Auth.rx_authCallback(observer: observer))
+    func rx_createUserWithEmail(email: String, password: String) -> Single<User> {
+        return Single<User>.create { single in
+            self.createUser(withEmail: email, password: password, completion: Auth.rx_authCallback(single: single))
             return Disposables.create()
         }
     }
 
-    func rx_sendPasswordResetWithEmail(email: String) -> Observable<Void> {
-        return Observable.create { (observer: AnyObserver<Void>) -> Disposable in
+    func rx_sendPasswordResetWithEmail(email: String) -> Completable {
+        return Completable.create(subscribe: { (completable) -> Disposable in
             self.sendPasswordReset(withEmail: email) { (error) in
                 if let error = error {
-                    observer.onError(error)
+                    completable(.error(error))
                 } else {
-                    observer.onNext(())
-                    observer.onCompleted()
+                    completable(.completed)
                 }
             }
-
             return Disposables.create()
-        }
+        })
     }
 
     // MARK: - Signing out
-    func rx_signOut() -> Observable<Void> {
-        return Observable.create { (observer: AnyObserver<Void>) -> Disposable in
+    func rx_signOut() -> Completable {
+        return Completable.create(subscribe: { (completable) -> Disposable in
             do {
                 try self.signOut()
-                observer.onNext(())
-                observer.onCompleted()
+                completable(.completed)
             } catch (let error) {
-                observer.onError(error)
+                completable(.error(error))
             }
-
             return Disposables.create()
-        }
+        })
     }
 
     // MARK: - Multi-provider support
-    func rx_providersForEmail(email: String) -> Observable<[String]?> {
-        return Observable.create { (observer: AnyObserver<[String]?>) -> Disposable in
+    func rx_providersForEmail(email: String) -> Single<[String]?> {
+        return Single.create(subscribe: { (single) -> Disposable in
             self.fetchProviders(forEmail: email) { (providers, error) in
                 if let error = error {
-                    observer.onError(error)
+                    single(.error(error))
                 } else {
-                    observer.onNext(providers)
-                    observer.onCompleted()
+                    single(.success(providers))
                 }
             }
 
             return Disposables.create()
-        }
+        })
     }
 
     // MARK: - Helper methods
-    private static func rx_authCallback(observer: AnyObserver<User?>) -> AuthResultCallback {
+    private static func rx_authCallback(single: @escaping ((SingleEvent<User>) -> ())) -> AuthResultCallback {
         return { (user: User?, error: Error?) in
-            if let error = error {
-                observer.onError(error)
-            } else {
-                observer.onNext(user)
-                observer.onCompleted()
+            guard let user = user else {
+                single(.error(error ?? NSError(domain: "FirebaseRxSwiftExtensions", code: ErrorCodeUnknownError,
+                                               userInfo: nil)))
+                return;
             }
+            single(.success(user))
         }
     }
 }
