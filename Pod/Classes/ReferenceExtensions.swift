e1e3ec92f121d27cb27a6cd1c7cc7c5f8d0226cf
Michael Kuck <me@michael-kuck.com>
2017-08-14 16:20:24 +0800

    Use Singles instead of Observables where possible for DB References

--- Pod/Classes/ReferenceExtensions.swift
+++ Pod/Classes/ReferenceExtensions.swift
@@ -8,53 +8,52 @@ import RxSwift
 
 public extension DatabaseReference {
     // MARK: - Updating references
-    func rx_updateChildValues(values: [NSObject: AnyObject]) -> Observable<DatabaseReference> {
-        return Observable.create { (observer: AnyObserver<DatabaseReference>) in
-            self.updateChildValues(values, withCompletionBlock: DatabaseReference.rx_setCallback(observer: observer))
+    func rx_updateChildValues(values: [String: Any]) -> Single<DatabaseReference> {
+        return Single.create(subscribe: { (single) -> Disposable in
+            self.updateChildValues(values, withCompletionBlock: DatabaseReference.rx_setSingleCallback(single: single))
             return Disposables.create()
-        }
+        })
     }
 
-    func rx_setValue(value: AnyObject?, priority: AnyObject? = nil) -> Observable<DatabaseReference> {
-        return Observable.create { (observer: AnyObserver<DatabaseReference>) -> Disposable in
+    func rx_setValue(value: AnyObject?, priority: AnyObject? = nil) -> Single<DatabaseReference> {
+        return Single.create(subscribe: { (single) -> Disposable in
             if let priority = priority {
-                self.setValue(value, andPriority: priority, withCompletionBlock: DatabaseReference.rx_setCallback(observer: observer))
+                self.setValue(value, andPriority: priority, withCompletionBlock: DatabaseReference.rx_setSingleCallback(single: single))
             } else {
-                self.setValue(value, withCompletionBlock: DatabaseReference.rx_setCallback(observer: observer))
+                self.setValue(value, withCompletionBlock: DatabaseReference.rx_setSingleCallback(single: single))
             }
 
             return Disposables.create()
-        }
+        })
     }
 
-    func rx_setPriority(priority: AnyObject?) -> Observable<DatabaseReference> {
-        return Observable.create { (observer: AnyObserver<DatabaseReference>) in
-            self.setPriority(priority, withCompletionBlock: DatabaseReference.rx_setCallback(observer: observer))
+    func rx_setPriority(priority: AnyObject?) -> Single<DatabaseReference> {
+        return Single.create(subscribe: { (single) -> Disposable in
+            self.setPriority(priority, withCompletionBlock: DatabaseReference.rx_setSingleCallback(single: single))
             return Disposables.create()
-        }
+        })
     }
 
-    func rx_removeValue() -> Observable<DatabaseReference> {
-        return Observable.create { (observer: AnyObserver<DatabaseReference>) in
-            self.removeValue(completionBlock: DatabaseReference.rx_setCallback(observer: observer))
+    func rx_removeValue() -> Single<DatabaseReference> {
+        return Single.create(subscribe: { (single) -> Disposable in
+            self.removeValue(completionBlock: DatabaseReference.rx_setSingleCallback(single: single))
             return Disposables.create()
-        }
+        })
     }
 
     // MARK: - Transactions
-    func rx_runTransactionBlock(block: @escaping ((MutableData) -> TransactionResult), withLocalEvents localEvents: Bool = false) -> Observable<(Bool, DataSnapshot?)> {
-        return Observable.create { (observer: AnyObserver<(Bool, DataSnapshot?)>) in
+    func rx_runTransactionBlock(block: @escaping ((MutableData) -> TransactionResult), withLocalEvents localEvents: Bool = false) -> Single<(Bool, DataSnapshot?)> {
+        return Single.create(subscribe: { (single) -> Disposable in
             self.runTransactionBlock(block, andCompletionBlock: { (error, isCommitted, snapshot) in
                 if let error = error {
-                    observer.onError(error)
+                    single(.error(error))
                 } else {
-                    observer.onNext((isCommitted, snapshot))
-                    observer.onCompleted()
+                    single(.success(isCommitted, snapshot))
                 }
             }, withLocalEvents: localEvents)
 
             return Disposables.create()
-        }
+        })
     }
 
     // MARK: - Disconnect handling
@@ -63,12 +62,12 @@ public extension DatabaseReference {
             if let priority = priority {
                 self.onDisconnectSetValue(value,
                     andPriority: priority,
-                    withCompletionBlock: DatabaseReference.rx_setCallback(observer: observer))
+                    withCompletionBlock: DatabaseReference.rx_setObservableCallback(observer: observer))
 
                 return Disposables.create()
             } else {
                 self.onDisconnectSetValue(value,
-                    withCompletionBlock: DatabaseReference.rx_setCallback(observer: observer))
+                    withCompletionBlock: DatabaseReference.rx_setObservableCallback(observer: observer))
 
                 return Disposables.create()
             }
@@ -78,7 +77,7 @@ public extension DatabaseReference {
     func rx_onDisconnectUpdateValue(values: [NSObject: AnyObject]) -> Observable<DatabaseReference> {
         return Observable.create { (observer) -> Disposable in
             self.onDisconnectUpdateChildValues(values,
-                withCompletionBlock: DatabaseReference.rx_setCallback(observer: observer))
+                withCompletionBlock: DatabaseReference.rx_setObservableCallback(observer: observer))
 
             return Disposables.create()
         }
@@ -86,19 +85,29 @@ public extension DatabaseReference {
 
     func rx_onDisconnectRemoveValue() -> Observable<DatabaseReference> {
         return Observable.create { (observer) -> Disposable in
-            self.onDisconnectRemoveValue(completionBlock: DatabaseReference.rx_setCallback(observer: observer))
+            self.onDisconnectRemoveValue(completionBlock: DatabaseReference.rx_setObservableCallback(observer: observer))
             return Disposables.create()
         }
     }
 
     // MARK: - Helper methods
-    private static func rx_setCallback(observer: AnyObserver<DatabaseReference>) -> ((Error?, DatabaseReference) -> Void) {
+    private static func rx_setObservableCallback(observer: AnyObserver<DatabaseReference>) -> ((Error?, DatabaseReference) -> Void) {
         return { (error: Error?, reference: DatabaseReference) in
             if let error = error {
                 observer.onError(error)
             } else {
                 observer.onNext(reference)
                 observer.onCompleted()
+            }
+        }
+    }
+
+    private static func rx_setSingleCallback(single: @escaping ((SingleEvent<DatabaseReference>) -> ())) -> ((Error?, DatabaseReference) -> Void) {
+        return { (error: Error?, reference: DatabaseReference) in
+            if let error = error {
+                single(.error(error))
+            } else {
+                single(.success(reference))
             }
         }
     }
